5ae99e86cbeb0fc41be7b3c933cd07cbe9d7e7e7
eddywindvogel <62585064+eddywindvogel@users.noreply.github.com>
2020-03-27 13:07:28 +0200

    Add files via upload

---
+++ Fake News/Fake News_files/wb14_fakenews.js
@@ -0,0 +1,11 @@
+   
+   $(document).ready(function()
+   {
+      $(document).on('click','.ThemeableMenu1-navbar-collapse.in',function(e)
+      {
+         if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) 
+         {
+            $(this).collapse('hide');
+         }
+      });
+   });
